088b788e70a9af7509df333b6e7bf4544cbb91c4
samikhan Mohmand <samigit45@gmail.com>
2024-03-18 04:05:19 -0700

    initial commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// let imgskhan=new Array("../images/backgroundkhan.jpg.jpg","../images/gallry7.jpg.jpg","../images/back.jpg.jpg");
+
+// let  i=0;
+// let timer;
+
+
+// function backgroundShow(){
+//     if(i>3){
+//     i=0;
+//     }
+    
+//     document.getElementById("#first-section").innerHTML="<img src='" + imgskhan[i] +"'>";
+//     i++;
+//     timer=window.setTimeout(backgroundShow,1000);
+// }
